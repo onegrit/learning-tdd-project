9c509a1297a40faab7a2e88c4f7394ce3c6f1bbd
root <root@db.localdomain>
2025-06-29 16:02:10 +0800

    refactor: created Money and Portfolio modules; improved tests

---
+++ js/portfolio.mjs
@@ -0,0 +1,17 @@
+import { Money } from "./money.mjs"; // Use ES6 module import
+
+export class Portfolio{
+  constructor(){
+    this.moneys = [];
+  }
+  add(...moneys){
+    this.moneys = this.moneys.concat(moneys);
+  }
+
+  evaluate(currency){
+    let total = this.moneys.reduce((sum,money)=>{
+      return sum + money.amount;
+    }, 0);
+    return new Money(total, currency);
+  }
+}
---
+++ js/test_money.mjs
@@ -0,0 +1,62 @@
+import * as assert from 'assert';
+import { Money } from './money.mjs';
+import { Portfolio } from './portfolio.mjs';
+
+class MoneyTest {
+  testMultiplication() {
+    let tenEuros = new Money(10, "EUR")
+    let twentyEuros = new Money(20, "EUR")
+    assert.deepStrictEqual(tenEuros.times(2), twentyEuros, 'Expected 20 euros');
+  }
+
+  testDivision() {
+    let originalMoney = new Money(4002.0, "KRW");
+    let actualMoneyAfterDivision = originalMoney.divide(4);
+    let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+    assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision,
+      `Expected: ${JSON.stringify(expectedMoneyAfterDivision)}, got: ${JSON.stringify(actualMoneyAfterDivision)}`);
+  }
+
+  testAddition() {
+    let tenEuros = new Money(10, "EUR");
+    let twentyEuros = new Money(20, "EUR");
+    let thirtyEuros = new Money(30, "EUR");
+    let portfolio = new Portfolio();
+    portfolio.add(tenEuros, twentyEuros);
+    assert.deepStrictEqual(portfolio.evaluate("EUR"), thirtyEuros,
+      `Expected: ${JSON.stringify(thirtyEuros)}, got: ${JSON.stringify(portfolio.evaluate("EUR"))}`);
+  }
+
+  getAllTestMethods() {
+    let moneyPrototype = MoneyTest.prototype;
+    let allProps = Object.getOwnPropertyNames(moneyPrototype);
+    let testMethods = allProps.filter(p => {
+      return typeof moneyPrototype[p] === 'function' && p.startsWith('test');
+    })
+
+    return testMethods;
+  }
+
+  runAllTests() {
+    let testMethods = this.getAllTestMethods();
+    testMethods.forEach(m => {
+      console.log(`Running test: ${m}`);
+      let method = Reflect.get(this, m);
+      try {
+        Reflect.apply(method, this, []);
+      } catch (e) {
+        if (e instanceof assert.AssertionError) {
+          console.error(`Test ${m} failed: ${e.message}`);
+        }
+        else {
+          throw e;
+        }
+      }
+    });
+  }
+}
+
+new MoneyTest().runAllTests();
+
+
+
